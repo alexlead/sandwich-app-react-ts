4d9d15669350ca8988c0f85bec9cc8e1940568a6
Alexander Lead <a.lead@yandex.ru>
2023-12-06 11:44:26 +0300

    homework 05.12.2023

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,19 +7,21 @@ const Counter: React.FC = (): JSX.Element => {
     const counter = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch();
 
-    const handleMinus = () => {
-        dispatch({ type: 'counter/minus', payload: 1 })
+    const handleMinus = (num: number) => {
+        dispatch({ type: 'counter/minus', payload: num || 1 })
     }
 
-    const handlePlus = () => {
-        dispatch({ type: 'counter/plus', payload: 1 })
+    const handlePlus = (num: number) => {
+        dispatch({ type: 'counter/plus', payload: num || 1 })
     }
 
   return (
     <div>
         <div>Counter: {counter} </div>
-        <button onClick={handleMinus}>Minus</button>
-        <button onClick={handlePlus}>Plus</button>
+        <button onClick={()=>handleMinus(10)}>Minus 10</button>
+        <button onClick={()=>handleMinus(1)}>Minus</button>
+        <button onClick={()=>handlePlus(1)}>Plus</button>
+        <button onClick={()=>handlePlus(10)}>Plus 10</button>
     </div>
   )
 }
